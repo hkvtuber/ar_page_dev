daf1a23542f09560cbb7aefbd4bd5aef89135802
hkvtuber <>
2021-10-09 03:01:18 +0800

    add html

---
+++ js/index.js
@@ -0,0 +1,4 @@
+function appendAR(path) {
+    var text = '<model-viewer alt="A 3D model " src="' + path + '" camera-controls environment-image="neutral" shadow-intensity="1"></model-viewer>';
+    document.getElementById("ar").insertAdjacentHTML('afterbegin', text);
+}
